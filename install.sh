0d9fc6dc590a3e1857639cee2dcb859d9e8f361f
watchman <watchman@matrix.com>
2024-02-19 15:29:00 +0800

    hey

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-#设置定时任务，每5分钟
+#设置定时任务，每15分钟
 rm -f cornjob
 cFolder="$(pwd)"
 #设定定时任务
-echo "*/5 * * * * $cFolder/wallpaper.sh  > /dev/null 2>&1" | crontab
+echo "*/15 * * * * $cFolder/wallpaper.sh  > /dev/null 2>&1" | crontab
 #获得一次壁纸
 ./wallpaper.sh  >> /dev/null 2>&1
